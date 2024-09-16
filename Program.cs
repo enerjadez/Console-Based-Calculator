2ba772bfd64d1233097bb7b86c9352ad6f56cf88
enerjadez <enerjadezn@gmail.com>
2024-09-16 19:16:59 +0200

    Calculator logic

--- Program.cs
+++ Program.cs
@@ -10,6 +10,37 @@ namespace Console_Based_Calculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Enter first number:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter second number:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter operation (+, -, *, /):");
+            char operation = Console.ReadLine()[0];
+
+            double result = 0;
+            switch (operation)
+            {
+                case '+':
+                    result = num1 + num2;
+                    break;
+                case '-':
+                    result = num1 - num2;
+                    break;
+                case '*':
+                    result = num1 * num2;
+                    break;
+                case '/':
+                    result = num1 / num2;
+                    break;
+                default:
+                    Console.WriteLine("Invalid operation");
+                    return;
+            }
+
+            Console.WriteLine("Result: " + result);
+            Console.ReadLine();
         }
     }
 }
